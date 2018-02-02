49964473e32442e36c458d42b611fb4193ee9039
Nina Y <nyuchi@risd.edu>
2018-02-02 16:18:36 -0500

    stuff

---
+++ final/script.js
@@ -0,0 +1,56 @@
+jQuery(function(){
+  var j = jQuery;
+  var addInput = '#qty';
+  var n = 1;
+  j(addInput).val(n);
+  j('.plus').on('click', function(){
+    j(addInput).val(++n);
+  })
+
+  j('.min').on('click', function(){
+    if (n >= 1) {
+      j(addInput).val(--n);
+    } else {
+    }
+  });
+});
+
+
+function myFunction() {
+    alert("Why would you buy "+ jQuery("#qty").val() + " of these? Reevaluate your life." );
+}
+
+function FunctionOne() {
+    document.getElementById("myDropdownOne").classList.toggle("show");
+}
+
+function FunctionTwo() {
+    document.getElementById("myDropdownTwo").classList.toggle("show");
+}
+
+function FunctionThree() {
+    document.getElementById("myDropdownThree").classList.toggle("show");
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
--- p5/sketch.js
+++ p5/sketch.js
@@ -1,27 +1,56 @@
+// function setup(){
+//   createCanvas(1000, 700, WEBGL);
+// }
+
+// function draw(){
+//   background(0);
+
+//   var locY = (mouseY / height - 0.5) * (-2);
+//   var locX = (mouseX / width - 0.5) * 2;
+
+//   ambientLigt(30);
+//   directionalLight(200, 0, 0, 0.25, 0.25, 0.25);
+//   pointLight(200, 0, 200, locX, locY, 0);
+//   pointLight(200, 200, 200, -locX, -locY, 0);
+
+//   push();
+//   translate(-250, 0, 0);
+//   rotateZ(frameCount * 0.02);
+//   rotateX(frameCount * 0.02);
+//   specularMaterial(200);
+//   box(200, 150, 100);
+//   pop();
+
+//   translate(250, 0, 0);
+//   ambientMaterial(250);
+//   sphere(120, 64);
+// }
+
 function setup(){
-  createCanvas(1000, 700, WEBGL);
+  createCanvas(1500, 800, WEBGL);
 }
 
 function draw(){
-  background(0);
+  background(250);
+  var radius = width;
+  
+  //drag to move the world.
+  orbitControl();
 
-  var locY = (mouseY / height - 0.5) * (-2);
-  var locX = (mouseX / width - 0.5) * 2;
-
-  ambientLigt(30);
-  directionalLight(200, 0, 0, 0.25, 0.25, 0.25);
-  pointLight(200, 0, 200, locX, locY, 0);
-  pointLight(200, 200, 200, -locX, -locY, 0);
-
-  push();
-  translate(-250, 0, 0);
-  rotateZ(frameCount * 0.02);
-  rotateX(frameCount * 0.02);
-  specularMaterial(200);
-  box(200, 150, 100);
-  pop();
-
-  translate(250, 0, 0);
-  ambientMaterial(250);
-  sphere(120, 64);
+  normalMaterial();
+  translate(0, 0, -600);
+  for(var i = 0; i <= 12; i++){
+    for(var j = 0; j <= 12; j++){
+      push();
+      var a = j/12 * PI;
+      var b = i/12 * PI;
+      translate(sin(2 * a) * radius * sin(b), cos(b) * radius / 2 , cos(2 * a) * radius * sin(b));    
+      if(j%2 === 0){
+        sphere(50);
+      }else{
+        sphere(30);
+      }
+      pop();
+    }
+  }
 }
